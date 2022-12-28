fe7219161906bd51b783886c05c43ffe17c61d99
Aman <aman@ericsson.com>
2022-12-28 17:04:57 +0530

    Problem Statement Name : Multiples of 3 or 5
    Problem Statement Name : Multiples of 3 or 5 Solution Added.

---
+++ solutions_vault/vault/solution_1.py
@@ -0,0 +1,20 @@
+"""
+    Problem Statement Name : Multiples of 3 or 5
+    
+    Description : 
+        If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+        Find the sum of all the multiples of 3 or 5 below 1000.
+        
+    Answer : 234168
+
+"""
+
+
+def solution(r: int, multiple_1: int, multiple_2: int) -> int:
+    
+    return sum([x for x in range(r) if x%multiple_1 == 0 or x%multiple_2 == 0])
+
+
+if __name__ == '__main__':
+    
+    print(solution(1000, 3, 5))
